c57a07e8d2805902de8a68795cfcc92a0d63e8c3
moshkov_n <ryawi.whill@gmail.com>
2024-05-16 12:31:22 +0500

    Changed lines

--- bspu_parser/parser.py
+++ bspu_parser/parser.py
@@ -3,13 +3,10 @@ from bs4 import BeautifulSoup
 
 
 def parse_teachers_fio():
-
     request = requests.get("https://bspu.ru/sveden/employees")
-
     soup = BeautifulSoup(request.content, 'html.parser')
 
     university_data = soup.find("div", id="collapseTwo")
-
     colleague_data = soup.find("div", id="collapseThree")
 
     university_fio = university_data.find_all("td", itemprop="fio")
