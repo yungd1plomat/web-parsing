3846a73f28331bd997823acbd7472ea2e1bc3fef
yungd1plomat <aynur.musin.06@mail.ru>
2024-05-17 02:18:35 +0500

    reorganize files and classes

--- intellect_search/search.py
+++ intellect_search/search.py
@@ -1,56 +1,48 @@
 from sentence_transformers import SentenceTransformer, util
+from dbconnector.dbconnector import DbContext
 
-# Созданные руками, тестовые данные
-data = [
-    {"author": "Старцева Оксана Геннадиевна", "title": "Анализ данных с использованием Python"},
-    {"author": "Абдрахманова Флорида Ривхатовна", "title": "Машинное обучение и глубокое обучение"},
-    {"author": "Абдуллин Рамиль Раисович", "title": "Введение в нейронные сети"},
-    {"author": "Анастасьев Алексей Геннадьевич", "title": "Обработка естественного языка"},
-    {"author": "Амирова Зарема Канзафаровна", "title": "Естественные духовные скрепы"},
-    {"author": "Амирова Людмила Александровна", "title": "Введение в высшую математику"},
-    {"author": "Амирова Оксана Георгиевна", "title": "Абстракция у млекопитающих"},
-    {"author": "Биккинин Ирек Анасович", "title": "Абстракция беспилотников и птиц"},
-    {"author": "Бикметов Рустам Фаритович", "title": "Абстракция беспилотников для самых маленьких"},
-    {"author": "Валеева Гульнара Рашитовна", "title": "Машинное обучение и глубокий математический анализ"},
-    {"author": "Валеева Лиана Фанитовна", "title": "Анализ данных с использованием 1С"},
-]
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-# Получение эмбеддингов для всех заголовков
-titles = [entry["title"] for entry in data]
-title_embeddings = model.encode(titles, convert_to_tensor=True)
-
-"""
-Выполняет семантический поиск статей по заданному запросу.
-
-:param query: Строка запроса для поиска
-:param data: Список словарей с данными статей
-:param model: Модель SentenceTransformer для получения эмбеддингов
-:param title_embeddings: Эмбеддинги заголовков статей
-:param threshold: Порог сходства для определения совпадений
-:return: Список найденных статей, удовлетворяющих критерию сходства
-"""
-def search_articles(query, data, model, title_embeddings, threshold=0.5):
-    query_embedding = model.encode(query, convert_to_tensor=True)
-    similarities = util.pytorch_cos_sim(query_embedding, title_embeddings)[0]
+class IntelliSearch:
+    def __init__(self):
+        self.__model = SentenceTransformer('all-MiniLM-L6-v2')
+        self.__database = DbContext()
     
-    results = []
-    for idx, similarity in enumerate(similarities):
-        if similarity >= threshold:
-            results.append({
-                "author": data[idx]["author"],
-                "title": data[idx]["title"],
-                "similarity": similarity.item()
-            })
-    
-    # Сортировка результатов по убыванию сходства
-    results.sort(key=lambda x: x["similarity"], reverse=True)
-    return results
+    """
+    Выполняет семантический поиск статей по заданному запросу.
 
-query = input("Введите название статьи для поиска: ")
-results = search_articles(query, data, model, title_embeddings)
+    :param query: Строка запроса для поиска
+    :param data: Список словарей с данными статей
+    :param model: Модель SentenceTransformer для получения эмбеддингов
+    :param title_embeddings: Эмбеддинги заголовков статей
+    :param threshold: Порог сходства для определения совпадений
+    :return: Список найденных статей, удовлетворяющих критерию сходства
+    """
+    def __search_articles(self, query, data, title_embeddings, threshold=0.5):
+        query_embedding = self.__model.encode(query, convert_to_tensor=True)
+        similarities = util.pytorch_cos_sim(query_embedding, title_embeddings)[0]
+        
+        results = []
+        for idx, similarity in enumerate(similarities):
+            if similarity >= threshold:
+                results.append({
+                    "authors": data[idx]["authors"],
+                    "article_name": data[idx]["article_name"],
+                    "similarity": similarity.item()
+                })
+        
+        # Сортировка результатов по убыванию сходства
+        results.sort(key=lambda x: x["similarity"], reverse=True)
+        return results
 
-print("Результаты поиска:")
-for result in results:
-    print(f"Автор: {result['author']}, Название: {result['title']}, Сходство: {result['similarity']:.2f}")
+    def __get_embeddings(self, articles):
+        
+        # Получение эмбеддингов для всех заголовков
+        names = [entry["article_name"] for entry in articles]
+        name_embeddings = self.__model.encode(names, convert_to_tensor=True)
+        return name_embeddings
+
+    def search_articles(self, query):
+        articles = self.__database.get_articles()
+        embeddings = self.__get_embeddings(articles)
+        results = self.__search_articles(query, articles, embeddings)
+        return results
+
