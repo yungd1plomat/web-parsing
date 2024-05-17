8f0dea33ee46d5a0e49fa24ca8c75f62b952065f
yungd1plomat <aynur.musin.06@mail.ru>
2024-05-17 12:18:02 +0500

    change search engine

--- intellect_search/search.py
+++ intellect_search/search.py
@@ -1,22 +1,15 @@
 from fuzzywuzzy import fuzz, process
 from dbconnector.dbconnector import DbContext
+from fuzzywuzzy import process
 
 class IntelliSearch:
     def __init__(self):
         self.__database = DbContext()
-    
-    def search_articles(self, query, threshold=90):
+
+    # Поиск в массиве статей с помощью fuzz.WRatio()
+    def search_articles(self, query):
+        # Получаем список статей
         articles = self.__database.get_articles()
-        results = []
-        for entry in articles:
-            authors = entry['authors']
-            name = entry['article_name']
-            link = entry['link']
-            ratio = fuzz.partial_ratio(query, name)
-            if ratio >= threshold:
-                results.append({'authors': authors, 'name': name, 'link': link, 'similarity': ratio})
-
-
-        results.sort(key=lambda x: x["similarity"], reverse=True)
+        # Ищем топ 10 похожих статьи по запросу
+        results = process.extract(query, articles, limit=10)
         return results
-
--- intellisearch.py
+++ intellisearch.py
@@ -1,6 +1,10 @@
 from intellect_search.search import IntelliSearch
+import logging
+
+logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.INFO)
 
 query = input('Введите название для поиска: ')
 intelli_search = IntelliSearch()
 result = intelli_search.search_articles(query)
-print(result)
+for article in result:
+    logging.info(f'Авторы: {article[0]["authors"]}, Статья: {article[0]["article_name"]}, Ссылка: {article[0]["link"]}')
