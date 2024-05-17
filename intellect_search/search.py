1716369fd4e6e85f788f57dbbc802e338cf57a7a
yungd1plomat <aynur.musin.06@mail.ru>
2024-05-17 10:22:47 +0500

    change intellect search logic

--- intellect_search/search.py
+++ intellect_search/search.py
@@ -1,48 +1,22 @@
-from sentence_transformers import SentenceTransformer, util
+from fuzzywuzzy import fuzz, process
 from dbconnector.dbconnector import DbContext
 
 class IntelliSearch:
     def __init__(self):
-        self.__model = SentenceTransformer('all-MiniLM-L6-v2')
         self.__database = DbContext()
     
-    """
-    Выполняет семантический поиск статей по заданному запросу.
-
-    :param query: Строка запроса для поиска
-    :param data: Список словарей с данными статей
-    :param model: Модель SentenceTransformer для получения эмбеддингов
-    :param title_embeddings: Эмбеддинги заголовков статей
-    :param threshold: Порог сходства для определения совпадений
-    :return: Список найденных статей, удовлетворяющих критерию сходства
-    """
-    def __search_articles(self, query, data, title_embeddings, threshold=0.5):
-        query_embedding = self.__model.encode(query, convert_to_tensor=True)
-        similarities = util.pytorch_cos_sim(query_embedding, title_embeddings)[0]
-        
-        results = []
-        for idx, similarity in enumerate(similarities):
-            if similarity >= threshold:
-                results.append({
-                    "authors": data[idx]["authors"],
-                    "article_name": data[idx]["article_name"],
-                    "similarity": similarity.item()
-                })
-        
-        # Сортировка результатов по убыванию сходства
-        results.sort(key=lambda x: x["similarity"], reverse=True)
-        return results
-
-    def __get_embeddings(self, articles):
-        
-        # Получение эмбеддингов для всех заголовков
-        names = [entry["article_name"] for entry in articles]
-        name_embeddings = self.__model.encode(names, convert_to_tensor=True)
-        return name_embeddings
-
-    def search_articles(self, query):
+    def search_articles(self, query, threshold=90):
         articles = self.__database.get_articles()
-        embeddings = self.__get_embeddings(articles)
-        results = self.__search_articles(query, articles, embeddings)
+        results = []
+        for entry in articles:
+            authors = entry['authors']
+            name = entry['article_name']
+            link = entry['link']
+            ratio = fuzz.partial_ratio(query, name)
+            if ratio >= threshold:
+                results.append({'authors': authors, 'name': name, 'link': link, 'similarity': ratio})
+
+
+        results.sort(key=lambda x: x["similarity"], reverse=True)
         return results
 
--- intellisearch.py
+++ intellisearch.py
@@ -1,5 +1,6 @@
 from intellect_search.search import IntelliSearch
 
+query = input('Введите название для поиска: ')
 intelli_search = IntelliSearch()
-result = intelli_search.search_articles('test')
+result = intelli_search.search_articles(query)
 print(result)
